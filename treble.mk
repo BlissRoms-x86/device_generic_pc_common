f108c8cab52c7e724f03c5c861af794e8cc54e7a
Unknown <electrikjesus@gmail.com>
2017-11-19 16:47:34 -0500

    more updated to treble

--- treble.mk
+++ treble.mk
@@ -2,6 +2,7 @@
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service
 
 # HWComposer HAL
 PRODUCT_PACKAGES += \
@@ -9,9 +10,8 @@ PRODUCT_PACKAGES += \
 
 # Audio HAL
 PRODUCT_PACKAGES += \
-    android.hardware.audio@1.0-impl \
-    android.hardware.audio@1.0-service \
-    android.hardware.audio.effect@1.0-impl \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
     android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl
 
@@ -21,9 +21,13 @@ PRODUCT_PACKAGES += \
 
 # Camera HAL
 PRODUCT_PACKAGES += \
-    camera.device@3.2-impl \
-    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl
     android.hardware.camera.provider@2.4-service
+
+# DRM HAL
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl
 
 # DumpState HAL
 PRODUCT_PACKAGES += \
@@ -31,8 +35,12 @@ PRODUCT_PACKAGES += \
     android.hardware.dumpstate@1.0-service
 
 # Gatekeeper HAL
-#PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl
+
+# GNSS HAL
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl
 
 # Health HAL
 PRODUCT_PACKAGES += \
@@ -47,8 +55,13 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl
 
 # Memtrack HAL
-#PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl
+
+# NFC HAL
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0-impl \
+    android.hardware.nfc@1.0-service
 
 # Power HAL
 PRODUCT_PACKAGES += \
@@ -62,11 +75,19 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 
+# Thermal HAL
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl
+
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-impl \
-    android.hardware.usb@1.0-service
+		android.hardware.usb@1.0-service
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
 
 # Wifi HAL
-#PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
